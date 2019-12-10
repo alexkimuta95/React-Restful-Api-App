b0e327010a547b675b039f9a7ac75f5418f1c88e
Alex Kimutai <billroberts2001@gmail.com>
2019-12-10 19:46:09 +0300

    The react app that interatcs with Rest api

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>this is the angular app that has been implemented by using gatsby</p>
+        </React.Fragment>
+    )
+}
